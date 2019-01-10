bffcd48b1435c2a83416e40f797f9161b185fec5
Victor Mac <victor.gx15@gmail.com>
2019-01-10 12:33:12 +0100

    Test on HTML page succeded

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 L15 is a Java microservice application
 
 Pour executer le truc:
--mvn compile
--mvn package
--java -jar target/L15bank-0.0.1-SNAPSHOT.jar
+- mvn package ou mvn compile
+- ./mvnw spring-boot:run
 
 ## Suivre ces tutos:
 
--- src/main/java/com/victor/antoine/L15/L15bank/controller/Greeting.java
+++ src/main/java/com/victor/antoine/L15/L15bank/controller/Greeting.java
@@ -2,6 +2,7 @@ package com.victor.antoine.L15.L15bank.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,5 +14,11 @@ public class Greeting {
     @RequestMapping(value="/Produits", method=RequestMethod.GET)
     public String listeProduits() {
         return "Un exemple de produit";
+    }
+    
+    @GetMapping("/hello")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
