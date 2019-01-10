811adfcb5b0c8adae5fce02b0d56cb6e1e60f396
Victor Mac <victor.gx15@gmail.com>
2019-01-10 09:29:43 +0100

    change for spring

---
+++ src/main/java/com/victor/antoine/L15/L15bank/L15bankApplication.java
@@ -0,0 +1,14 @@
+package com.victor.antoine.L15.L15bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L15bankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L15bankApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/victor/antoine/L15/L15bank/L15bankApplicationTests.java
@@ -0,0 +1,17 @@
+package com.victor.antoine.L15.L15bank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class L15bankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
