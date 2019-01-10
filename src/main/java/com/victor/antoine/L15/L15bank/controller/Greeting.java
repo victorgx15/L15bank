ff342fa1dcdb6fed06eedf050e6e8afbc32315db
Victor Mac <victor.gx15@gmail.com>
2019-01-10 11:42:59 +0100

    Test Greeting

--- src/main/java/com/victor/antoine/L15/L15bank/controller/Greeting.java
+++ src/main/java/com/victor/antoine/L15/L15bank/controller/Greeting.java
@@ -3,14 +3,15 @@ package com.victor.antoine.L15.L15bank.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class Greeting {
-	
-	@RequestMapping("/hello")
-	public String greeting() {
-        return "greeting";
+    @RequestMapping(value="/Produits", method=RequestMethod.GET)
+    public String listeProduits() {
+        return "Un exemple de produit";
     }
 }
