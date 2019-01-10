3f81f9399d7871ac27780994f0c2723a798eb008
lantoine <antoine.lepers@dauphine.eu>
2019-01-10 11:28:16 +0100

    Correct type mistake in operation

--- src/main/java/com/victor/antoine/L15/L15bank/model/Operation.java
+++ src/main/java/com/victor/antoine/L15/L15bank/model/Operation.java
@@ -2,16 +2,16 @@ package com.victor.antoine.L15.L15bank.model;
 
 public class Operation {
     private int id;
-    private int iban_source;
-    private int type_dest;
+    private String iban_source;
+    private String iban_dest;
     private double value;
     private String date;
     private String Label;
 
-    public Operation(int id, int iban_source, int type_dest, double value, String date, String label) {
+    public Operation(int id, String iban_source, String iban_dest, double value, String date, String label) {
         this.id = id;
         this.iban_source = iban_source;
-        this.type_dest = type_dest;
+        this.iban_dest = iban_dest;
         this.value = value;
         this.date = date;
         Label = label;
@@ -21,12 +21,12 @@ public class Operation {
         return id;
     }
 
-    public int getIban_source() {
+    public String getIban_source() {
         return iban_source;
     }
 
-    public int getType_dest() {
-        return type_dest;
+    public String getIban_dest() {
+        return iban_dest;
     }
 
     public double getValue() {
