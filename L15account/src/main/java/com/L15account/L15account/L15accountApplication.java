05615f6748da59140d1f493edd1212d96d91dd28
Victor Mac <victor.gx15@gmail.com>
2019-01-16 12:57:22 +0100

    account added

---
+++ L15account/src/main/java/com/L15account/L15account/L15accountApplication.java
@@ -0,0 +1,13 @@
+package com.L15account.L15account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L15accountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L15accountApplication.class, args);
+	}
+
+}
