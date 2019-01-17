58f5ed4df4b924222894226de7128d8f76b59900
Victor Mac <victor.gx15@gmail.com>
2019-01-17 23:42:50 +0100

    some rogue transfers added

--- L15operation/src/main/resources/data.sql
+++ L15operation/src/main/resources/data.sql
@@ -1,2 +1,11 @@
 INSERT INTO Operation(id, ibanSrc, ibanDest, value, date, label, type) VALUES
-(101,'FR562039' , 'FR4539',20,'13/01/20180', 'pizza', 'VIREMENT');
+(101,'FR1' , 'LY1',20,'13/01/20180', 'pizza', 'VIREMENT'),
+(102,'FR1' , 'LY1',10,'13/01/20180', 'pizza', 'CB'),
+(104,'FR1' , 'LY1',23000,'13/01/20180', 'Rolex', 'CB'),
+(105,'FR1' , 'LY1',67,'13/01/20180', 'Fouquets', 'CHEQUE'),
+
+(106,'LY1' , 'FR1',26729,'13/01/20180', 'Campagne 2017', 'VIREMENT'),
+(107,'LY1' , 'FR1',167432,'13/01/20180', 'Campagne 2012', 'VIREMENT'),
+(108,'LY1' , 'FR1',567131,'13/01/20180', 'Campagne 2007', 'VIREMENT'),
+(109,'LY1' , 'FR1',96437,'13/01/20180', 'Fouquets', 'VIREMENT'),
+;
